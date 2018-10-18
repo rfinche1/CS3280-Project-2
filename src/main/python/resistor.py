b73b101d4f984bbbff4439291cd140d5a1665aaf
Ras Fincher <rfinche1@my.westga.edu>
2018-10-18 09:11:43 -0400

    Added validateFiveBandResistorColorList and tests
    Added validateFiveBandResistorColorList and tests. Also added more tests for resistors with four bands.

--- src/main/python/resistor.py
+++ src/main/python/resistor.py
@@ -13,23 +13,28 @@ def validateColorsList(colors):
     @return true if colors is a valid list of colors,
     false otherwise
     '''
+
     if colors is None:
-        raise ValueError("Colors list cannot be None.")
+        raise ValueError('Colors list cannot be None.')
 
     if colors == []:
-        raise ValueError("The colors list cannot be empty.")
+        raise ValueError('The colors list cannot be empty.')
 
     if len(colors) < 4:
-        raise ValueError("The colors list must contain at least four colors.")
+        raise ValueError('The colors list must contain at least four colors.')
 
     if len(colors) > 5:
-        raise ValueError("The colors list must contain at least four colors.")
+        raise ValueError('The colors list must contain at least four colors.')
+
+    colorsContainsOnlyValidColors(colors)
 
     if len(colors) == 4:
-        (band1, band2, band3, band4) = colors
-        for band in colors:
-            if not isValidColor(band):
-                raise ValueError(band + " is not a valid color")
+        validateFourBandResistorColorsList(colors)
+
+    if len(colors) == 5:
+        validateFiveBandResistorColorsList(colors)
+
+    return True
 
 
 def isValidColor(color):
@@ -43,3 +48,94 @@ def isValidColor(color):
     false otherwise
     '''
     return color in valid_colors
+
+
+def isFirstBandValid(color):
+    '''
+    Returns true if the color of the first band is
+    a valid color for that band, and false otherwise.
+
+    @param color    the color of the band
+
+    @preconditions color != 'gold' or color != 'silver'
+
+    @return true if the band is a valid color,
+    false otherwise
+    '''
+    invalidBandColors = ['gold', 'silver']
+    return color not in invalidBandColors
+
+
+def isSecondBandValid(color):
+    '''
+    Returns true if the color of the second band is
+    a valid color for that band, and false otherwise
+
+    @param color    the color of the band
+
+    @preconditions color != 'gold' or color != 'silver'
+
+    @return true if the band is a valid color,
+    false otherwise
+    '''
+    invalidBandColors = ['gold', 'silver']
+    return color not in invalidBandColors
+
+
+def isFifthBandValid(color):
+    '''
+    Returns true if the color of the fifth band is
+    a valid color for that band, and false otherwise
+
+    @param color    the color of the band
+
+    @preconditions color != 'black' and color != 'orange'
+                    and color != 'yellow' and color != 'white'
+
+    @return true if the band is a valid color,
+    false otherwise
+    '''
+    invalidBandColors = ['black', 'orange', 'yellow', 'white']
+    return color not in invalidBandColors
+
+
+def validateFourBandResistorColorsList(colors):
+    '''
+    TODO: Spec
+    '''
+    (band1, band2, band3, band4) = colors
+
+    if not isFirstBandValid(band1):
+        raise ValueError(band1 + ' is not allowed in the first band.')
+
+    if not isSecondBandValid(band2):
+        raise ValueError(band2 + ' is not allowed in the second band.')
+
+    return True
+
+
+def validateFiveBandResistorColorsList(colors):
+    '''
+    TODO: Spec
+    '''
+    (band1, band2, band3, band4, band5) = colors
+
+    if not isFirstBandValid(band1):
+        raise ValueError(band1 + ' is not allowed in the first band.')
+
+    if not isSecondBandValid(band2):
+        raise ValueError(band2 + ' is not allowed in the second band.')
+
+    if not isFifthBandValid(band5):
+        raise ValueError(band5 + ' is not allowed in the fifth band.')
+
+    return True
+
+
+def colorsContainsOnlyValidColors(colors):
+    '''
+    TODO: Spec
+    '''
+    for band in colors:
+        if not isValidColor(band):
+            raise ValueError(str(band) + ' is not a valid color.')
--- src/unittest/python/validate_colors_list_tests.py
+++ src/unittest/python/validate_colors_list_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from resistor import validateColorsList
+from resistor import *
 
 
 class TestValidateColorsList(unittest.TestCase):
@@ -28,6 +28,74 @@ class TestValidateColorsList(unittest.TestCase):
         colors = ['black', 'brown', 'red', 'blue', 'green', 'white']
         self.assertRaises(ValueError, validateColorsList, colors)
 
+    def testValidateColorsListWithFourColorsWhenFirstColorIsNone(self):
+        colors = [None, 'brown', 'red', 'blue']
+        self.assertRaises(ValueError, validateColorsList, colors)
+
     def testInvalidFirstBandInColorsListWithFourColors(self):
         colors = ['maroon', 'brown', 'red', 'blue']
         self.assertRaises(ValueError, validateColorsList, colors)
+
+    def testInvalidSecondBandInColorsListWithFourColors(self):
+        colors = ['black', 'aqua', 'red', 'blue']
+        self.assertRaises(ValueError, validateColorsList, colors)
+
+    def testInvalidThirdBandInColorsListWithFourColors(self):
+        colors = ['black', 'brown', 'taupe', 'blue']
+        self.assertRaises(ValueError, validateColorsList, colors)
+
+    def testInvalidFourthColorInColorsListWithFourColors(self):
+        colors = ['black', 'brown', 'red', 'mauve']
+        self.assertRaises(ValueError, validateColorsList, colors)
+
+    def testAllFourColorsInvalidInColorsListWithFourColors(self):
+        colors = ['maroon', 'aqua', 'taupe', 'mauve']
+        self.assertRaises(ValueError, validateColorsList, colors)
+
+    def testGoldInFirstBandInColorsListWithFourColors(self):
+        colors = ['gold', 'brown', 'red', 'blue']
+        self.assertRaises(ValueError, validateColorsList, colors)
+
+    def testSilverInFirstBandInColorsListWithFourColors(self):
+        colors = ['silver', 'brown', 'red', 'blue']
+        self.assertRaises(ValueError, validateColorsList, colors)
+
+    def testGoldInSecondBandInColorsListWithFourColors(self):
+        colors = ['black', 'gold', 'red', 'blue']
+        self.assertRaises(ValueError, validateColorsList, colors)
+
+    def testSilverInSecondBandInColorsListWithOthersValide(self):
+        colors = ['black', 'silver', 'red', 'blue']
+        self.assertRaises(ValueError, validateColorsList, colors)
+
+    def testGoldInFirstBandSilverInSecondAndLastTwoValidColors(self):
+        colors = ['gold', 'silver', 'red', 'blue']
+        self.assertRaises(ValueError, validateColorsList, colors)
+
+    def testSilverInFirstBandGoldInSecondAndLastTwoValidColors(self):
+        colors = ['silver', 'gold', 'red', 'blue']
+        self.assertRaises(ValueError, validateColorsList, colors)
+
+    def testSilverInThirdBandInColorsList(self):
+        self.assertTrue(validateColorsList(
+            ['black', 'brown', 'silver', 'red']))
+
+    def testGoldInThirdBandInColorsList(self):
+        self.assertTrue(validateColorsList(
+            ['black', 'brown', 'gold', 'red']))
+
+    def testSilverInFourthBandInColorsList(self):
+        self.assertTrue(validateColorsList(
+            ['black', 'brown', 'red', 'silver']))
+
+    def testGoldInFourthBandInColorsList(self):
+        self.assertTrue(validateColorsList(
+            ['black', 'brown', 'red', 'gold']))
+
+    def testValidFourBandResistor(self):
+        self.assertTrue(validateColorsList(
+            ['black', 'brown', 'red', 'blue']))
+
+    def testValidFiveBandResistor(self):
+        self.assertTrue(validateColorsList(
+            ['black', 'brown', 'red', 'blue', 'brown']))
